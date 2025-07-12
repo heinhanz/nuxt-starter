8e1f8a31243a494c7b3f1508347a2201abc714ed
heinhanz <heinzweisenahlx@gmail.com>
2025-07-12 22:04:55 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,29 +8,8 @@ definePageMeta({
 </script>
 
 <template>
-  <Container>
-    <Hero
-      :title="page.hero.title"
-      :description="page.hero.description"
-      :image="page.hero.image"
-      :imageAlt="page.hero.imageAlt"
-      :buttons="page.hero.buttons"
-    ></Hero>
-    <Logos :title="page.logos.title" :icons="page.logos.icons"></Logos>
-    <Features
-      :title="page.features.title"
-      :description="page.features.description"
-      :items="page.features.items"
-    ></Features>
-    <Testimonials
-      :title="page.testimonials.title"
-      :description="page.testimonials.description"
-      :items="page.testimonials.items"
-    ></Testimonials>
-    <Cta
-      :title="page.cta.title"
-      :description="page.cta.description"
-      :buttons="page.cta.buttons"
-    ></Cta>
-  </Container>
+  <div>
+    <h1>XWEBMARKET.EU</h1>
+    <p>Will will come soon</p>
+  </div>
 </template>
